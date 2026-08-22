e32d25e88f40c7081be1280c3ffe74bcd7522fa0
Akcodet7 <162880573+Akcodet7@users.noreply.github.com>
2026-08-23 01:48:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -6,23 +6,25 @@ class Solution {
             adj[edges[i][0]].push_back({edges[i][2], edges[i][1]});
             adj[edges[i][1]].push_back({edges[i][2], edges[i][0]});
         }
-        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        //priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        set<pair<int,int>> s;
         vector<int> dist(V,INT_MAX);
         dist[src]=0;
-        pq.push({0,src});
-        while(!pq.empty()) {
-            pair<int,int> topp = pq.top();
-            int d=topp.first;
-            int node=topp.second;
-             pq.pop();
-            if(d>dist[node]) continue;
-           
+        s.insert({0,src});
+        while(!s.empty()) {
+            auto it=*(s.begin());
+            int d=it.first;
+            int node=it.second;
+            s.erase(it);
             for(int i=0; i<adj[node].size(); i++) {
                 int weight=adj[node][i].first;
                 int neighbor=adj[node][i].second;
                 if(dist[neighbor] > (weight+dist[node])) {
+                    if(dist[neighbor]!=INT_MAX) {
+                        s.erase({dist[neighbor],neighbor});
+                    }
                     dist[neighbor] = weight+dist[node];
-                    pq.push({dist[neighbor], neighbor});
+                    s.insert({dist[neighbor], neighbor});
                 }
             }
         }

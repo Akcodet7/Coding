3c7e63f38ebfafbed97f3e0c59d3edaa9a7c1bc1
Akcodet7 <162880573+Akcodet7@users.noreply.github.com>
2026-08-22 17:52:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int,int>>> adj(V);
+        for(int i=0; i<edges.size(); i++) {
+            adj[edges[i][0]].push_back({edges[i][2], edges[i][1]});
+            adj[edges[i][1]].push_back({edges[i][2], edges[i][0]});
+        }
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(V,INT_MAX);
+        dist[src]=0;
+        pq.push({0,src});
+        while(!pq.empty()) {
+            pair<int,int> topp = pq.top();
+            int d=topp.first;
+            int node=topp.second;
+             pq.pop();
+            if(d>dist[node]) continue;
+           
+            for(int i=0; i<adj[node].size(); i++) {
+                int weight=adj[node][i].first;
+                int neighbor=adj[node][i].second;
+                if(dist[neighbor] > (weight+dist[node])) {
+                    dist[neighbor] = weight+dist[node];
+                    pq.push({dist[neighbor], neighbor});
+                }
+            }
+        }
+        return dist;
+    }
+};

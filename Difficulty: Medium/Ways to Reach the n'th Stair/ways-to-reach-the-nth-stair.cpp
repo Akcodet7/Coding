bcf8a1d2b6d38123c3312ba6916ce7edfbdfb86d
Akcodet7 <162880573+Akcodet7@users.noreply.github.com>
2026-08-24 03:33:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    
+    int f(vector<int>&dp, int n) {
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] =  f(dp, n-1)+f(dp, n-2);
+    }
+  
+    int countWays(int n) {
+        vector<int> dp(n+1, -1);
+        return f(dp,n);
+    }
+};

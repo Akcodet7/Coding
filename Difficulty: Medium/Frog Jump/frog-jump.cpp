6c914d2bacf23d76d0b3d8a8ef4ae75bb1c7e923
Akcodet7 <162880573+Akcodet7@users.noreply.github.com>
2026-08-24 16:06:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -8,9 +8,18 @@ class Solution {
         int two = f(dp, n-2, heights) + abs(heights[n]-heights[n-2]);
         return dp[n] = min(one,two);
     }
-  
-    int minCost(vector<int>& height) {
-        vector<int> dp(height.size(), -1);
-        return f(dp, height.size()-1, height);
+    int curri=0, ts, os;
+    int minCost(vector<int>& heights) {
+        int prev = 0;
+        int prev2 = 0;
+        for(int i=1; i<heights.size(); i++) {
+            os = prev + abs(heights[i]-heights[i-1]);
+            ts = INT_MAX;
+            if(i>1) ts = prev2 +abs(heights[i]-heights[i-2]);
+            curri = min(os, ts);
+            prev2 = prev;
+            prev = curri;
+        }
+        return curri;
     }
 };

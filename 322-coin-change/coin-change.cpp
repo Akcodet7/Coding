6f680c0e254ec105b1b3d2b653cb29ae8e883dc1
Akcodet7 <162880573+Akcodet7@users.noreply.github.com>
2026-09-06 18:57:04 +0530

    Time: 847 ms (5.00%) | Memory: 48.8 MB (42.40%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
 
     int f(int ind, vector<int>&coins, int amount, vector<vector<int>>&dp) {
-        if(amount<0) return INT_MAX;
         if(ind==coins.size()) {
             if(amount==0) return 0;
             return INT_MAX;
@@ -19,8 +18,27 @@ public:
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int> (amount+1,-1));
-        if (f(0, coins, amount, dp)==INT_MAX) return -1;
-        return f(0, coins, amount, dp);
+        vector<vector<int>> dp(coins.size(), vector<int> (amount+1,0));
+
+        for(int j=1; j<=amount; j++) {
+            if(j%coins[0]==0)
+            dp[0][j] = j/coins[0];
+            else dp[0][j] = INT_MAX;
+        }
+        for(int i=1; i<coins.size(); i++) {
+            for(int j=1; j<=amount; j++) {
+                int minn = INT_MAX;
+                int a=0;
+                while(coins[i]*a<=j) {
+                    if(dp[i-1][j-coins[i]*a]<INT_MAX) minn = min(minn, a+dp[i-1][j - coins[i]*a]);
+                    a++;
+                }
+                dp[i][j] = minn;
+            }
+        }
+        if(dp[coins.size()-1][amount]==INT_MAX) return -1;
+        return dp[coins.size()-1][amount];
+            // if (f(0, coins, amount, dp)==INT_MAX) return -1;
+            // return f(0, coins, amount, dp);
     }
 };
